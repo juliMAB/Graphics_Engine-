d3b5e1b18b5409cf3b6da4fb3d978064773967b3
juliMAB <julian6aguirre@hotmail.com>
2021-08-19 09:48:28 -0300

    Added base Game class.

---
+++ Game/src/Game/Game.cpp
@@ -0,0 +1,32 @@
+#include "Game.h"
+#include <time.h>
+#include <iostream>
+
+namespace Graficos1 {
+
+	Game::Game()
+	{
+
+	}
+	Game::~Game() 
+	{
+		
+	}
+	void Game::Start() 
+	{
+		
+	}
+	void Game::Play() 
+	{
+		
+	}
+
+	void Game::Update() 
+	{
+		
+	}
+	void Game::End() 
+	{
+
+	}
+}
---
+++ Game/src/Game/Game.h
@@ -0,0 +1,19 @@
+#ifndef GAME_H
+#define GAME_H
+//#include "GameBase/GameBase.h"
+
+namespace Graficos1 {
+
+	class Game {
+		
+	public:
+		Game();
+		~Game();
+		void Play();
+		void Start();
+		void Update();
+		void End();
+	};
+
+}
+#endif
