68545b965a9632935c6f04fde88bf42a24d0055d
juliMAB <julian6aguirre@hotmail.com>
2021-08-19 09:56:02 -0300

    Added main.

---
+++ Game/src/main.cpp
@@ -0,0 +1,12 @@
+#include "Game/Game.h"
+using namespace Graficos1;
+#include <iostream>
+int main(void) {
+	Game* g = new Game();
+	if (g) {
+		g->Start();
+		g->Play();
+		g->End();
+		delete g;
+	}
+}
