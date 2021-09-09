9e44755f123b78c31bdb9805eee7fb19d3a1d434
Federico Olive <FedericoOlive@gmail.com>
2021-09-09 10:00:09 -0300

    Add DrawElements.

--- GraphicsEngine/src/Renderer/Renderer.cpp
+++ GraphicsEngine/src/Renderer/Renderer.cpp
@@ -99,30 +99,40 @@ void Renderer::LinkShaders() {
 	glDeleteShader(fragmentShader);
 }
 void Renderer::DrawTriangle() {
-	glUseProgram(programShader);
+	/*glUseProgram(programShader);
 	glBindVertexArray(VAO); // seeing as we only have a single VAO there's no need to bind it every time, but we'll do so to keep things a bit more organized
-	glDrawArrays(GL_TRIANGLES, 0, 3);
+	glDrawArrays(GL_TRIANGLES, 0, 3);*/
+
+	glUseProgram(programShader);
+	glBindVertexArray(VAO);
+	glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
+
+	
 }
 void Renderer::SetTriangle() {
 	float vertices[] = {
-		-0.5f, -0.5f, 0.0f, // left  
-		 0.5f, -0.5f, 0.0f, // right 
-		 0.0f,  0.5f, 0.0f  // top   
+		 -0.5f, -0.5f,
+		  0.5f, -0.5f,
+		  0.5f,  0.5f
 	};
-
+	unsigned int indices[] = {  // note that we start from 0!
+		0, 1, 2,  // first Triangle
+	};
+	
 	glGenVertexArrays(1, &VAO);
 	glGenBuffers(1, &VBO);
-
+	glGenBuffers(1, &EBO);
 	glBindVertexArray(VAO);
 
 	glBindBuffer(GL_ARRAY_BUFFER, VBO);
 	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
 
+	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
+	glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
+
 	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
 	glEnableVertexAttribArray(0);
-
 	glBindBuffer(GL_ARRAY_BUFFER, 0);
-
 	glBindVertexArray(0);
 }
 void Renderer::SetQuad() {
--- GraphicsEngine/src/Renderer/Renderer.h
+++ GraphicsEngine/src/Renderer/Renderer.h
@@ -15,7 +15,7 @@ private:
 		"out vec4 FragColor;\n"
 		"void main()\n"
 		"{\n"
-		"   FragColor = vec4(1.0f, 0.0f, 0.0f, 1.0f);\n"
+		"   FragColor = vec4(0.0f, 1.0f, 0.0f, 1.0f);\n"
 		"}\n\0";
 	Window* _window;
 	unsigned int buffer;
