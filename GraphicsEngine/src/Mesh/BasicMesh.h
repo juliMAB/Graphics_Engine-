4fa2c453364792536ed74d13f579cd08c4b5cf22
juliMAB <julian6aguirre@hotmail.com>
2022-05-10 21:35:24 -0300

    Fix Assimp dll.

--- GraphicsEngine/src/Mesh/BasicMesh.h
+++ GraphicsEngine/src/Mesh/BasicMesh.h
@@ -24,11 +24,11 @@
 #include <assimp/postprocess.h> // Post processing flags
 #include "../Exports/Exports.h"
 
-#include "ogldev_util.h"
-#include "ogldev_math_3d.h"
-#include "ogldev_texture.h"
-#include "ogldev_world_transform.h"
-#include "ogldev_material.h"
+//#include "ogldev_util.h"
+//#include "ogldev_math_3d.h"
+//#include "ogldev_texture.h"
+//#include "ogldev_world_transform.h"
+//#include "ogldev_material.h"
 
 class GraficosEngine_API IRenderCallbacks
 {
@@ -42,89 +42,9 @@ class GraficosEngine_API BasicMesh
 public:
     BasicMesh() {};
 
-    ~BasicMesh();
-
-    bool LoadMesh(const std::string& Filename);
-
-    void Render(IRenderCallbacks* pRenderCallbacks = NULL);
-
-    void Render(uint DrawIndex, uint PrimID);
-
-    void Render(uint NumInstances, const Matrix4f* WVPMats, const Matrix4f* WorldMats);
-
-    WorldTrans& GetWorldTransform() { return m_worldTransform; }
-
-    const Material& GetMaterial();
-
-    void GetLeadingVertex(uint DrawIndex, uint PrimID, Vector3f& Vertex);
+    ~BasicMesh() {};
 
 private:
-    void Clear();
-
-    bool InitFromScene(const aiScene* pScene, const std::string& Filename);
-
-    void CountVerticesAndIndices(const aiScene* pScene, uint& NumVertices, uint& NumIndices);
-
-    void ReserveSpace(uint NumVertices, uint NumIndices);
-
-    void InitAllMeshes(const aiScene* pScene);
-
-    void InitSingleMesh(const aiMesh* paiMesh);
-
-    bool InitMaterials(const aiScene* pScene, const std::string& Filename);
-
-    void PopulateBuffers();
-
-    void LoadTextures(const string& Dir, const aiMaterial* pMaterial, int index);
-
-    void LoadDiffuseTexture(const string& Dir, const aiMaterial* pMaterial, int index);
-
-    void LoadSpecularTexture(const string& Dir, const aiMaterial* pMaterial, int index);
-
-    void LoadColors(const aiMaterial* pMaterial, int index);
-
-#define INVALID_MATERIAL 0xFFFFFFFF
-
-    enum BUFFER_TYPE {
-        INDEX_BUFFER = 0,
-        POS_VB = 1,
-        TEXCOORD_VB = 2,
-        NORMAL_VB = 3,
-        WVP_MAT_VB = 4,  // required only for instancing
-        WORLD_MAT_VB = 5,  // required only for instancing
-        NUM_BUFFERS = 6
-    };
-
-    WorldTrans m_worldTransform;
-    GLuint m_VAO = 0;
-    GLuint m_Buffers[NUM_BUFFERS] = { 0 };
-
-    struct BasicMeshEntry {
-        BasicMeshEntry()
-        {
-            NumIndices = 0;
-            BaseVertex = 0;
-            BaseIndex = 0;
-            MaterialIndex = INVALID_MATERIAL;
-        }
-
-        uint NumIndices;
-        uint BaseVertex;
-        uint BaseIndex;
-        uint MaterialIndex;
-    };
-
-    std::vector<BasicMeshEntry> m_Meshes;
-    std::vector<Material> m_Materials;
-
-    // Temporary space for vertex stuff before we load them into the GPU
-    vector<Vector3f> m_Positions;
-    vector<Vector3f> m_Normals;
-    vector<Vector2f> m_TexCoords;
-    vector<uint> m_Indices;
-
-    const aiScene* m_pScene;
-    Assimp::Importer m_Importer;
 
 };
 
