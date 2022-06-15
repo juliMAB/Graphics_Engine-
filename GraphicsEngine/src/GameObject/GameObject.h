a1f4ec9b963e05dd46e4f3b81716baf1d928803d
juliMAB <julian6aguirre@hotmail.com>
2022-06-15 16:15:31 -0300

    Added GameObject class.

---
+++ GraphicsEngine/src/GameObject/GameObject.h
@@ -0,0 +1,32 @@
+#ifndef GAMEOBJECT_H
+#define GAMEOBJECT_H
+#include "Object/Object.h"
+#include "Transform/Transform.h"
+namespace JuliEngine
+{
+	class GraficosEngine_API GameObject : protected Object
+	{
+	public:
+		GameObject() { active = true; layer = 0; };
+		~GameObject() {};
+		Transform getTransform() { return transform; };
+		int getLayer() { return layer; };
+		bool getactive() { return active; };
+		string getTag() { return tag; };
+
+		void setLayer(int v) { layer = v; };
+		void setActive(bool v) { active = v; };
+		void setTag(string v) { tag = v; };
+
+	protected:
+		Transform transform;
+		int layer;
+		bool active;
+		string tag;
+	private:
+
+	};
+
+}
+
+#endif // !GAMEOBJECT_H
