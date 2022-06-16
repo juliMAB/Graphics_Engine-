610b6dbec6cf61af9928af65c055d0b2691a6539
juliMAB <julian6aguirre@hotmail.com>
2022-06-16 10:53:34 -0300

    Add isStatic bool to gameobject class.

--- GraphicsEngine/src/GameObject/GameObject.h
+++ GraphicsEngine/src/GameObject/GameObject.h
@@ -13,16 +13,19 @@ namespace JuliEngine
 		int getLayer() { return layer; };
 		bool getactive() { return active; };
 		string getTag() { return tag; };
+		bool getisStatic() { return isStatic; };
 
 		void setLayer(int v) { layer = v; };
 		void setActive(bool v) { active = v; };
 		void setTag(string v) { tag = v; };
+		void setisStatic(bool v) { isStatic = v; };
 
 	protected:
 		Transform transform;
 		int layer;
 		bool active;
 		string tag;
+		bool isStatic;
 	private:
 
 	};
